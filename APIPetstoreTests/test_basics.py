e7761d6e8d7d601e4226b3506e02792eb9016e3c
DMITRII VORONETS <dmivron620@gmail.com>
2025-04-25 23:02:30 +0500

    added post test

--- APIPetstoreTests/test_basics.py
+++ APIPetstoreTests/test_basics.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 baseURI = 'https://petstore.swagger.io/v2/pet/'
 petID = '2'
@@ -14,4 +13,30 @@ def test_get_pet_by_id_check_id():
     url = baseURI + petID
     resp = requests.get(url)
     data = resp.json()
-    assert data.get('id') == 2, 'Response is empty'
+    assert data.get('id') == 2, 'ID is wrong'
+
+
+def test_add_new_pet():
+    url = baseURI
+    body = {
+  "id": 191,
+  "category": {
+    "id": 0,
+    "name": "string"
+  },
+  "name": "Cuttie",
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "string"
+    }
+  ],
+  "status": "available"
+    }
+    resp = requests.post(url=url, json=body)
+    data = resp.json()
+    assert data.get('id') == 191, 'ID is wrong'
+    assert len(data) != 0, 'Response is empty'
