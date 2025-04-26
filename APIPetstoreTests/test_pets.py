35e71c90acebcac472e9c3c0748ab71cb90fdbe0
DMITRII VORONETS <dmivron620@gmail.com>
2025-04-26 21:34:08 +0500

    added a put test

--- APIPetstoreTests/test_pets.py
+++ APIPetstoreTests/test_pets.py
@@ -1,4 +1,4 @@
-from utils.myutils import getAPIData
+from utils.myutils import *
 from utils.confparser import *
 
 #baseURI = 'https://petstore.swagger.io/v2/pet/'
@@ -11,3 +11,11 @@ def test_getPetByID_response():
     data, status, time_taken = getAPIData(url)
     assert data.get('id') == int(petID), 'Wrong'
     assert status == 200, 'Wrong'
+
+# обновляем данные по pet
+def test_updatePet():
+    payload = {"id": int(petID),"category":{"id":0,"name":"string"},"name":"Zelda","photoUrls":["string"],"tags":[{"id":0,"name":"string"}],"status":"pending"}
+    data, status, time_taken = putData(baseURI, payload)
+    assert data.get('id') == int(petID)
+    assert data.get('name') == 'Zelda'
+    assert status == 200
--- utils/myutils.py
+++ utils/myutils.py
@@ -1,8 +1,17 @@
 import requests
 
+# get
 def getAPIData(url):
     response = requests.get(url)
     data = response.json()
     assert len(data) != 0, 'empty response'
     time_taken = response.elapsed.total_seconds()
     return data, response.status_code, time_taken
+
+# put
+def putData(url, body):
+    response = requests.post(url, json=body)
+    data = response.json()
+    time_taken = response.elapsed.total_seconds()
+    return data, response.status_code, time_taken
+
